c6f0531ec0b764f6e24943f67cc9db0dc2f86e16
ChikaKanu <chika.kanu@procompletions.com>
2019-07-01 23:45:25 +0100

    complete order

--- src/main/java/com/intellidigest/example/intellisolved/models/Order.java
+++ src/main/java/com/intellidigest/example/intellisolved/models/Order.java
@@ -1,5 +1,72 @@
 package com.intellidigest.example.intellisolved.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="orders")
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name="orderNumber")
+    private String orderNumber;
+
+    @Column(name="status")
+    private String status;
+
+    @JsonIgnoreProperties("product")
+    @Cascade(org.hibernate.annotations.CascadeType.DELETE)
+    @OneToMany(mappedBy="order", fetch=FetchType.LAZY)
+    private List<Product> products;
+
+    public Order(String status) {
+        this.id = id;
+        this.orderNumber = orderNumber;
+        this.status = status;
+        this.products = new ArrayList<>();
+    }
+
+    public Order(){};
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOrderNumber() {
+        DecimalFormat myFormatter = new DecimalFormat("ORD000000");
+        return myFormatter.format(id);
+    }
+
+    public void setOrderNumber(String orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
 }
 
