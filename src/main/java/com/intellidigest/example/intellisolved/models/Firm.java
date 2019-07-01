08f1c5550ce62b7e289ea7089c99b0f3e55326a9
ChikaKanu <chika.kanu@procompletions.com>
2019-07-01 14:01:10 +0100

    complete firm

--- src/main/java/com/intellidigest/example/intellisolved/models/Firm.java
+++ src/main/java/com/intellidigest/example/intellisolved/models/Firm.java
@@ -1,4 +1,92 @@
 package com.intellidigest.example.intellisolved.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="firms")
 public class Firm {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "postcode")
+    private String postcode;
+
+    @JsonIgnoreProperties("products")
+    @Cascade(org.hibernate.annotations.CascadeType.DELETE)
+    @OneToMany(mappedBy = "firm", fetch = FetchType.LAZY)
+    private List<Product> products;
+
+    public Firm(String name, String address, String city, String postcode) {
+        this.name = name;
+        this.address = address;
+        this.city = city;
+        this.postcode = postcode;
+        this.products = new ArrayList<>();
+    }
+
+    public Firm(){};
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
 }
