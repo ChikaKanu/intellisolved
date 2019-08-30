69d86f965ed5e2a703c8608c0274434d8241ad9c
ChikaKanu <chika.kanu@procompletions.com>
2019-08-30 07:08:13 +0100

    projection for order complete

--- intellisolvedBE/src/main/java/com/intellidigest/example/intellisolved/projections/OrderProjection.java
+++ intellisolvedBE/src/main/java/com/intellidigest/example/intellisolved/projections/OrderProjection.java
@@ -1,8 +1,15 @@
 package com.intellidigest.example.intellisolved.projections;
 
 import com.intellidigest.example.intellisolved.models.Order;
+import com.intellidigest.example.intellisolved.models.Product;
+import com.intellidigest.example.intellisolved.models.User;
 import org.springframework.data.rest.core.config.Projection;
 
 @Projection(name = "embedded", types = Order.class)
 public interface OrderProjection {
+   long getId();
+   String getOrderNumber();
+   String getStatus();
+   User getUser();
+   Product getProducts();
 }
