93e19d65053dd9639cb71aec170f3d32bcae22f7
ChikaKanu <chika.kanu@procompletions.com>
2019-08-30 06:54:32 +0100

    projection for store and user complete

--- intellisolvedBE/src/main/java/com/intellidigest/example/intellisolved/projections/StoreProjection.java
+++ intellisolvedBE/src/main/java/com/intellidigest/example/intellisolved/projections/StoreProjection.java
@@ -1,4 +1,16 @@
 package com.intellidigest.example.intellisolved.projections;
 
+import com.intellidigest.example.intellisolved.models.Store;
+import com.intellidigest.example.intellisolved.models.User;
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(name = "embedded", types = Store.class)
 public interface StoreProjection {
+   Long getId();
+   String getName();
+   String getAddress();
+   String getCity();
+   String getPostcode();
+   User getUser();
+
 }
--- intellisolvedBE/src/main/java/com/intellidigest/example/intellisolved/projections/UserProjection.java
+++ intellisolvedBE/src/main/java/com/intellidigest/example/intellisolved/projections/UserProjection.java
@@ -1,4 +1,24 @@
 package com.intellidigest.example.intellisolved.projections;
 
+import com.intellidigest.example.intellisolved.models.Order;
+import com.intellidigest.example.intellisolved.models.Product;
+import com.intellidigest.example.intellisolved.models.Store;
+import com.intellidigest.example.intellisolved.models.User;
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(name = "embedded", types = User.class)
 public interface UserProjection {
+   long getId();
+   String getFirstName();
+   String getSurname();
+   String getEmail();
+   String getUsername();
+   String getPassword();
+   String getPhone();
+   String getAddress();
+   String getCity();
+   String getPostcode();
+   Product getPreferredProducts();
+   Order getOrder();
+   Store getPreferredStore();
 }
